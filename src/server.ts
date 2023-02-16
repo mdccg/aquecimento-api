f16d896ace23326dbc0244f013a9a39a141d5dda
Lab 3 <matheusdanielgomess@gmail.com>
2023-02-15 21:53:37 -0400

    Introduced Newman and added math endpoints

--- src/server.ts
+++ src/server.ts
@@ -21,5 +21,29 @@ app.get('/pi', (req, res) => {
   return res.status(200).json({ pi: valorPi });
 });
 
+app.get('/soma/:parametro1/:parametro2', (req, res) => {
+  const { parametro1, parametro2 } = req.params;
+  const numero1 = Number(parametro1);
+  const numero2 = Number(parametro2);
+  if (!isNaN(numero1) && !isNaN(numero2)) {
+    return res.status(200).json({ soma: numero1 + numero2 });
+  } else {
+    return res.status(400).send('Números inválidos');
+  }
+});
+
+app.get('/divisao', (req, res) => {
+  const { numero1, numero2 } = req.query;
+  const n1 = Number(numero1);
+  const n2 = Number(numero2);
+  if (n2 === 0) {
+    return res.status(400).json({ mensagem: 'Divisor não pode ser zero' });
+  } else if (!isNaN(n1) && !isNaN(n2)) {
+    return res.status(200).json({ divisao: n1 / n2 });
+  } else {
+    return res.status(400).send('Números inválidos');
+  }
+});
+
 // Levanta o servidor
 app.listen(3001, () => console.log('Eu juro solenemente não fazer nada de bom'));
